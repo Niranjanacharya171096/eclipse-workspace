cf33e8f2eef9d331a90b6926296c872f5882ccb8
Niranjanacharya171096 <niranjanacharya171096@gmail.com>
2025-07-23 20:22:38 +0530

    modified commited

--- 06_selenium/1_Selenium_IBMudemyCourse/src/_05_extentreports/P169_SeleniumLoginTest.java
+++ 06_selenium/1_Selenium_IBMudemyCourse/src/_05_extentreports/P169_SeleniumLoginTest.java
@@ -18,66 +18,66 @@ import com.relevantcodes.extentreports.LogStatus;
 
 public class P169_SeleniumLoginTest {
 
-	private WebDriver driver;
-	private String baseUrl;
-	ExtentReports report;
-	ExtentTest test;
+  private WebDriver driver;
+  private String baseUrl;
+  ExtentReports report;
+  ExtentTest test;
 
-	@BeforeClass
-	public void beforeClass() {
-		baseUrl = "http://www.letskodeit.com/";
-		report = new ExtentReports("//Users//atomar//Desktop//logintest.html");
-		test = report.startTest("Verify Welcome Text");
-		driver = new FirefoxDriver();
-		test.log(LogStatus.INFO, "Browser Started...");
+  @BeforeClass
+  public void beforeClass() {
+    baseUrl = "http://www.letskodeit.com/";
+    report = new ExtentReports("//Users//atomar//Desktop//logintest.html");
+    test = report.startTest("Verify Welcome Text");
+    driver = new FirefoxDriver();
+    test.log(LogStatus.INFO, "Browser Started...");
 
-		// Maximize the browser's window
-		driver.manage().window().maximize();
-		test.log(LogStatus.INFO, "Browser Maximized");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get(baseUrl);
-		test.log(LogStatus.INFO, "Web application opened");
-	}
+    // Maximize the browser's window
+    driver.manage().window().maximize();
+    test.log(LogStatus.INFO, "Browser Maximized");
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    driver.get(baseUrl);
+    test.log(LogStatus.INFO, "Web application opened");
+  }
 
-	@Test
-	public void test1_validLoginTest() throws Exception {
-		WebElement signupLink = driver.findElement(By.id("comp-iiqg1vggactionTitle"));
-		signupLink.click();
-		test.log(LogStatus.INFO, "Clicked on signup link");
+  @Test
+  public void test1_validLoginTest() throws Exception {
+    WebElement signupLink = driver.findElement(By.id("comp-iiqg1vggactionTitle"));
+    signupLink.click();
+    test.log(LogStatus.INFO, "Clicked on signup link");
 
-		WebElement loginLink = driver.findElement(By.id("signUpDialogswitchDialogLink"));
-		loginLink.click();
-		test.log(LogStatus.INFO, "Clicked on login link");
+    WebElement loginLink = driver.findElement(By.id("signUpDialogswitchDialogLink"));
+    loginLink.click();
+    test.log(LogStatus.INFO, "Clicked on login link");
 
-		WebElement emailField = driver.findElement(By.xpath("//div[@id='memberLoginDialogemail']//input"));
-		emailField.sendKeys("test@email.com");
-		test.log(LogStatus.INFO, "Enter email");
+    WebElement emailField = driver.findElement(By.xpath("//div[@id='memberLoginDialogemail']//input"));
+    emailField.sendKeys("test@email.com");
+    test.log(LogStatus.INFO, "Enter email");
 
-		WebElement passwordField = driver.findElement(By.xpath("//div[@id='memberLoginDialogpassword']//input"));
-		passwordField.sendKeys("abcabc");
-		test.log(LogStatus.INFO, "Enter password");
+    WebElement passwordField = driver.findElement(By.xpath("//div[@id='memberLoginDialogpassword']//input"));
+    passwordField.sendKeys("abcabc");
+    test.log(LogStatus.INFO, "Enter password");
 
-		WebElement goButton = driver.findElement(By.id("memberLoginDialogsubmitButton"));
-		goButton.click();
-		test.log(LogStatus.INFO, "Clicked Go button");
+    WebElement goButton = driver.findElement(By.id("memberLoginDialogsubmitButton"));
+    goButton.click();
+    test.log(LogStatus.INFO, "Clicked Go button");
 
-		Thread.sleep(3000);
+    Thread.sleep(3000);
 
-		WebElement welcomeText = null;
+    WebElement welcomeText = null;
 
-		try {
-			welcomeText = driver.findElement(By.xpath("//div[text()='Hello test@email.com']"));
-		} catch (NoSuchElementException e) {
-			System.out.println(e.getMessage());
-		}
-		Assert.assertTrue(welcomeText != null);
-		test.log(LogStatus.PASS, "Verified Welcome Text");
-	}
+    try {
+      welcomeText = driver.findElement(By.xpath("//div[text()='Hello test@email.com']"));
+    } catch (NoSuchElementException e) {
+      System.out.println(e.getMessage());
+    }
+    Assert.assertTrue(welcomeText != null);
+    test.log(LogStatus.PASS, "Verified Welcome Text");
+  }
 
-	@AfterClass
-	public void afterClass() {
-		driver.quit();
-		report.endTest(test);
-		report.flush();
-	}
+  @AfterClass
+  public void afterClass() {
+    driver.quit();
+    report.endTest(test);
+    report.flush();
+  }
 }
