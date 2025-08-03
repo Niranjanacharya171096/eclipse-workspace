bc611bf088f03598ef87fc450e8d203632189281
Niranjanacharya171096 <niranjanacharya171096@gmail.com>
2025-08-03 18:07:46 +0530

    Update P23_ReverseFirstBiggestWord.java

--- 01_corejava/JavaInterviewPrograms/src/strings/P23_ReverseFirstBiggestWord.java
+++ 01_corejava/JavaInterviewPrograms/src/strings/P23_ReverseFirstBiggestWord.java
@@ -1,37 +1,36 @@
 package strings;
 
-public class P23_ReverseFirstBiggestWord {
+class P23_ReverseFirstBiggestWord {
   public static void main(String[] args) {
     String str = "Find the longest word and reverse it";
-    String result = reverseBiggestWord1(str);
+    String result = reverseLongestWord1(str);
     System.out.println(result);
 
-    String res = reverseBiggestWord2(str);
+    String res = reverseLongestWord2(str);
     System.out.println(res);
   }
 
-  public static String reverseBiggestWord1(String str) {
+  public static String reverseLongestWord1(String str) {
     String[] words = str.split(" ");
-    String biggest = "";
-
-    // Find the biggest word
+    String longestWord = "";
     for (String word : words) {
-      if (word.length() > biggest.length()) {
-        biggest = word;
+      if (word.length() >= longestWord.length()) {
+        longestWord = word;
       }
     }
+    System.out.println("Longest Word :: " + longestWord);
 
     // Manually reverse the biggest word
     String reversed = "";
-    for (int i = biggest.length() - 1; i >= 0; i--) {
-      reversed += biggest.charAt(i);
+    for (int i = longestWord.length() - 1; i >= 0; i--) {
+      reversed += longestWord.charAt(i);
     }
 
-    // Rebuild the string with the first occurrence of the biggest word reversed
+    // Rebuild the string with the first occurrence of the longest word reversed
     String result = "";
     boolean replaced = false;
     for (int i = 0; i < words.length; i++) {
-      if (!replaced && words[i].equals(biggest)) {
+      if (!replaced && words[i].equals(longestWord)) {
         result += reversed;
         replaced = true;
       } else {
@@ -46,19 +45,19 @@ public class P23_ReverseFirstBiggestWord {
     return result;
   }
 
-  public static String reverseBiggestWord2(String str) {
+  public static String reverseLongestWord2(String str) {
     String[] words = str.split("\\s+");
-    String biggest = "";
-
+    String longestWord = "";
     for (String word : words) {
-      if (word.length() > biggest.length()) {
-        biggest = word;
+      if (word.length() >= longestWord.length()) {
+        longestWord = word;
       }
     }
+    System.out.println("Longest Word :: " + longestWord);
+ 
+    StringBuilder reversed = new StringBuilder(longestWord).reverse();
 
-    StringBuilder reversed = new StringBuilder(biggest).reverse();
-
-    // Replace only the first occurrence of the biggest word
-    return str.replaceFirst("\\b" + biggest + "\\b", reversed.toString());
+    // Replace only the first occurrence of the longest word
+    return str.replaceFirst("\\b" + longestWord + "\\b", reversed.toString());
   }
 }
